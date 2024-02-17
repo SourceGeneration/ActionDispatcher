11a67fecc1f5ba53ea91473fc827095232c6eefa
肖昕 <xiao.xin@outlook.com>
2024-02-17 04:53:57 +0100

    rename

---
+++ src/SourceGeneration.ActionDispatcher/DependencyInjection/ActionDispatcherServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace SourceGeneration.ActionDispatcher;
+
+public static class ActionDispatcherServiceCollectionExtensions
+{
+    public static IServiceCollection AddActionDispatcher(this IServiceCollection services, ServiceLifetime serviceLifetime = ServiceLifetime.Scoped)
+    {
+        services.TryAdd(new ServiceDescriptor(typeof(ActionSubscriber), typeof(ActionSubscriber), serviceLifetime));
+        services.TryAdd(new ServiceDescriptor(typeof(IActionSubscriber), p => p.GetRequiredService<ActionSubscriber>(), serviceLifetime));
+        services.TryAdd(new ServiceDescriptor(typeof(IActionNotifier), p => p.GetRequiredService<ActionSubscriber>(), serviceLifetime));
+        services.TryAdd(new ServiceDescriptor(typeof(IActionDispatcher), typeof(ActionDispatcher), serviceLifetime));
+        return services;
+    }
+}
