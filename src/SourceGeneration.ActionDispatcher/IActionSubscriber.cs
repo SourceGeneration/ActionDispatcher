9e7c9eaa4bfd83f1b45ea6152c5d37160509d474
肖昕 <xiao.xin@outlook.com>
2024-11-18 17:00:55 +0800

    Update version

--- src/SourceGeneration.ActionDispatcher/IActionSubscriber.cs
+++ src/SourceGeneration.ActionDispatcher/IActionSubscriber.cs
@@ -7,6 +7,11 @@ public interface IActionSubscriber
     IDisposable Subscribe<TAction>(object? subscriber, ActionDispatchStatus status, Action<TAction, Exception?> callback) where TAction : notnull;
     IDisposable Subscribe(object? subscriber, ActionDispatchStatus status, Type[] actionTypes, Action<object, Exception?> callback);
 
+    IDisposable Subscribe<TAction>(object? subscriber, ActionDispatchStatus status, Func<TAction, Exception?, Task> callback) where TAction : notnull;
+    IDisposable Subscribe(object? subscriber, ActionDispatchStatus status, Type[] actionTypes, Func<object, Exception?, Task> callback);
+
+
+
     IDisposable Subscribe(object? subscriber, ActionDispatchStatus status, Action<object, Exception?> callback) => Subscribe<object>(subscriber, status, callback);
 
     IDisposable Subscribe<TAction>(object? subscriber, ActionDispatchStatus status, Action<TAction> callback) where TAction : notnull => Subscribe(subscriber, status, (TAction action, Exception? ex) => callback(action));
@@ -23,4 +28,24 @@ public interface IActionSubscriber
     IDisposable Subscribe<TAction>(Action<TAction, Exception?> callback) where TAction : notnull => Subscribe(null, ActionDispatchStatus.RanToCompletion, callback!);
     IDisposable Subscribe<TAction>(Action<TAction> callback) where TAction : notnull => Subscribe(null, ActionDispatchStatus.Successed, (TAction action, Exception? ex) => callback(action));
     IDisposable Subscribe<TAction>(Action callback) where TAction : notnull => Subscribe(null, ActionDispatchStatus.Successed, (TAction action, Exception? ex) => callback());
+
+
+
+    IDisposable Subscribe(object? subscriber, ActionDispatchStatus status, Func<object, Exception?, Task> callback) => Subscribe<object>(subscriber, status, callback);
+
+    IDisposable Subscribe<TAction>(object? subscriber, ActionDispatchStatus status, Func<TAction, Task> callback) where TAction : notnull => Subscribe(subscriber, status, (TAction action, Exception? ex) => callback(action));
+    IDisposable Subscribe<TAction>(object? subscriber, ActionDispatchStatus status, Func<Task> callback) where TAction : notnull => Subscribe(subscriber, status, (TAction action, Exception? ex) => callback());
+
+    IDisposable Subscribe<TAction>(object? subscriber, Func<TAction, Exception?, Task> callback) where TAction : notnull => Subscribe(subscriber, ActionDispatchStatus.RanToCompletion, callback!);
+    IDisposable Subscribe<TAction>(object? subscriber, Func<TAction, Task> callback) where TAction : notnull => Subscribe(subscriber, ActionDispatchStatus.Successed, (TAction action, Exception? ex) => callback(action));
+    IDisposable Subscribe<TAction>(object? subscriber, Func<Task> callback) where TAction : notnull => Subscribe(subscriber, ActionDispatchStatus.Successed, (TAction action, Exception? ex) => callback());
+
+    IDisposable Subscribe<TAction>(ActionDispatchStatus status, Func<TAction, Exception?, Task> callback) where TAction : notnull => Subscribe(null, status, callback);
+    IDisposable Subscribe<TAction>(ActionDispatchStatus status, Func<TAction, Task> callback) where TAction : notnull => Subscribe(null, status, (TAction action, Exception? ex) => callback(action));
+    IDisposable Subscribe<TAction>(ActionDispatchStatus status, Func<Task> callback) where TAction : notnull => Subscribe(null, status, (TAction action, Exception? ex) => callback());
+
+    IDisposable Subscribe<TAction>(Func<TAction, Exception?, Task> callback) where TAction : notnull => Subscribe(null, ActionDispatchStatus.RanToCompletion, callback!);
+    IDisposable Subscribe<TAction>(Func<TAction, Task> callback) where TAction : notnull => Subscribe(null, ActionDispatchStatus.Successed, (TAction action, Exception? ex) => callback(action));
+    IDisposable Subscribe<TAction>(Func<Task> callback) where TAction : notnull => Subscribe(null, ActionDispatchStatus.Successed, (TAction action, Exception? ex) => callback());
+
 }
