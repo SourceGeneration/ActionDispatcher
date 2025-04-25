ffea58ead621ae3a2a9ccfb932aeb65e9f69a703
肖昕 <xiao.xin@outlook.com>
2025-04-25 10:14:07 +0800

    add Notify extension method

--- src/SourceGeneration.ActionDispatcher/IActionNotifier.cs
+++ src/SourceGeneration.ActionDispatcher/IActionNotifier.cs
@@ -2,5 +2,7 @@
 
 public interface IActionNotifier
 {
+    void Notify(object action) => Notify(ActionDispatchStatus.Successed, action);
+    void Notify(object action, Exception exception) => Notify(ActionDispatchStatus.Faulted, action,exception);
     void Notify(ActionDispatchStatus status, object action, Exception? exception = null);
 }
