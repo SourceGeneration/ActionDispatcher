4f412389d762d05aacfc9e8a6ae195cfe0a5667c
肖昕 <xiao.xin@outlook.com>
2025-04-25 10:14:52 +0800

    add AddActionSubscriber method

--- src/SourceGeneration.ActionDispatcher/DependencyInjection/ActionDispatcherServiceCollectionExtensions.cs
+++ src/SourceGeneration.ActionDispatcher/DependencyInjection/ActionDispatcherServiceCollectionExtensions.cs
@@ -5,15 +5,34 @@ namespace SourceGeneration.ActionDispatcher;
 
 public static class ActionDispatcherServiceCollectionExtensions
 {
-    public static IServiceCollection AddActionDispatcher(this IServiceCollection services,
-        ServiceLifetime dispatcherLifetime = ServiceLifetime.Scoped,
-        ServiceLifetime subscriberLifetime = ServiceLifetime.Scoped)
+    public static IServiceCollection AddActionDispatcher(this IServiceCollection services, ServiceLifetime dispatcherLifetime = ServiceLifetime.Scoped, ServiceLifetime subscriberLifetime = ServiceLifetime.Scoped)
     {
+        services.Add(new ServiceDescriptor(typeof(IActionDispatcher), typeof(ActionDispatcher), dispatcherLifetime));
+
         services.TryAdd(new ServiceDescriptor(typeof(ActionSubscriber), typeof(ActionSubscriber), subscriberLifetime));
         services.TryAdd(new ServiceDescriptor(typeof(IActionSubscriber), p => p.GetRequiredService<ActionSubscriber>(), subscriberLifetime));
         services.TryAdd(new ServiceDescriptor(typeof(IActionNotifier), p => p.GetRequiredService<ActionSubscriber>(), subscriberLifetime));
 
-        services.TryAdd(new ServiceDescriptor(typeof(IActionDispatcher), typeof(ActionDispatcher), dispatcherLifetime));
         return services;
     }
+
+    public static IServiceCollection AddActionSubscriber(this IServiceCollection services, ServiceLifetime subscriberLifetime = ServiceLifetime.Scoped)
+    {
+        services.Add(new ServiceDescriptor(typeof(ActionSubscriber), typeof(ActionSubscriber), subscriberLifetime));
+        services.Add(new ServiceDescriptor(typeof(IActionSubscriber), p => p.GetRequiredService<ActionSubscriber>(), subscriberLifetime));
+        services.Add(new ServiceDescriptor(typeof(IActionNotifier), p => p.GetRequiredService<ActionSubscriber>(), subscriberLifetime));
+        return services;
+    }
+
+#if NET8_0_OR_GREATER
+    public static IServiceCollection AddKeyedActionSubscriber(this IServiceCollection services, object? serviceKey, ServiceLifetime subscriberLifetime = ServiceLifetime.Scoped)
+    {
+        services.Add(new ServiceDescriptor(typeof(ActionSubscriber), serviceKey, typeof(ActionSubscriber), subscriberLifetime));
+        services.Add(new ServiceDescriptor(typeof(IActionSubscriber), serviceKey, (sp, key) => sp.GetRequiredKeyedService<ActionSubscriber>(key), subscriberLifetime));
+        services.Add(new ServiceDescriptor(typeof(IActionNotifier), serviceKey, (sp, key) => sp.GetRequiredKeyedService<ActionSubscriber>(key), subscriberLifetime));
+        return services;
+    }
+
+#endif
+
 }
