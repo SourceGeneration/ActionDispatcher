41e8d39ce90325a8a9da3ca80691651b86ab6b4f
肖昕 <xiao.xin@outlook.com>
2025-05-06 13:13:40 +0800

    remove default implementation, use extension method instead

--- src/SourceGeneration.ActionDispatcher/IActionNotifier.cs
+++ src/SourceGeneration.ActionDispatcher/IActionNotifier.cs
@@ -2,7 +2,5 @@
 
 public interface IActionNotifier
 {
-    void Notify(object action) => Notify(ActionDispatchStatus.Successed, action);
-    void Notify(object action, Exception exception) => Notify(ActionDispatchStatus.Faulted, action,exception);
     void Notify(ActionDispatchStatus status, object action, Exception? exception = null);
 }
---
+++ src/SourceGeneration.ActionDispatcher/IActionNotifierExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace SourceGeneration.ActionDispatcher;
+
+public static class IActionNotifierExtensions
+{
+    public static void Notify(this IActionNotifier notifier, object action) => notifier.Notify(ActionDispatchStatus.Successed, action);
+    public static void Notify(this IActionNotifier notifier, object action, Exception exception) => notifier.Notify(ActionDispatchStatus.Faulted, action, exception);
+}
